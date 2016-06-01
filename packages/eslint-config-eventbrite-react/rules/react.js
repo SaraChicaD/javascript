47cd7094c65e2e4b1a542d6876e603bb23dc23a0
Ben Ilegbodu <ben.ilegbodu@eventbrite.com>
2016-06-01 15:52:35 -0700

    Add missing React & JSX eslint rules (#3)
    Also changing the peerDependencies back to using caret syntax since it's possible that we'll add new rules not available in previous versions of packages

--- packages/eslint-config-eventbrite-react/rules/react.js
+++ packages/eslint-config-eventbrite-react/rules/react.js
@@ -21,13 +21,13 @@ module.exports = {
         // http://eslint.org/docs/rules/jsx-quotes
         'jsx-quotes': 'error',
 
-        // True boolean props must pass {true} value
-        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
-        'react/jsx-boolean-value': ['error', 'always'],
-
         // Forbid propTypes: any, array, object
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
         'react/forbid-prop-types': 'error',
+
+        // True boolean props must pass {true} value
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+        'react/jsx-boolean-value': ['error', 'always'],
 
         // Validate closing bracket location in JSX is aligned with opening tag
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-closing-bracket-location.md
@@ -41,24 +41,85 @@ module.exports = {
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-equals-spacing.md
         'react/jsx-equals-spacing': 'error',
 
+        // Enforce that the first property should always be placed on a new line when the properties are spread over multiple lines
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-first-prop-new-line.md
+        'react/jsx-first-prop-new-line': ['error', 'multiline'],
+
         // Enforce event handler naming conventions in JSX: on* for props, handle* from functions
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-handler-names.md
         'react/jsx-handler-names': 'error',
+
+        // Enforce 4 space JSX tag indentation
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
+        'react/jsx-indent': 'error',
+
+        // Enforce 4 space JSX props indentation
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
+        'react/jsx-indent-props': 'error',
 
         // Validate JSX has key prop when in array or iterator
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
         'react/jsx-key': 'error',
 
+        // Limit maximum of props on a single line in JSX to 3
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-max-props-per-line.md
+        'react/jsx-max-props-per-line': ['error', {maximum: 3}],
+
+        // Prevent arrow functions & refs in a JSX prop (but still allow bind for now)
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
+        'react/jsx-no-bind': ['error', {
+            allowBind: true
+        }],
+
+        // Prevent usage of unsafe target="_blank" w/o rel="noopener noreferrer"
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md
+        'react/jsx-no-target-blank': 'error',
+
+        // Enforce PascalCase for user-defined JSX components
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
+        'react/jsx-pascal-case': 'error',
+
+        // Enforce that there's a space before the closing bracket of self-closing JSX elements
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-space-before-closing.md
+        'react/jsx-space-before-closing': 'error',
+
         // Warn when using "dangerous" JSX properties
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
         'react/no-danger': 'warn',
+
+        // Prevent multiple component definitions in a given file
+        // (stateless helper components are ok)
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
+        'react/no-multi-comp': ['error', {ignoreStateless: true}],
+
+        // Prevent using (legacy) string references in ref attribute
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-string-refs.md
+        'react/no-string-refs': 'error',
 
         // Require ES6 class declarations over React.createClass
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-es6-class.md
         'react/prefer-es6-class': 'error',
 
+        // Do not enforce stateless React Components to be written as a pure function
+        // TODO: Consider enabling in the future because they will benefit from
+        // future performance optimizations
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+        'react/prefer-stateless-function': 'off',
+
+        // Enforce that ES6 class returns a value for `render()` method
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
+        'react/require-render-return': 'error',
+
+        // Enforce that components are self-closed when they don't have content
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+        'react/self-closing-comp': 'error',
+
         // Enforce default component methods order
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
-        'react/sort-comp': 'error'
+        'react/sort-comp': 'error',
+
+        // Enforce multi-line JSX is wrapped in parentheses
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
+        'react/wrap-multilines': 'error'
     }
 };
